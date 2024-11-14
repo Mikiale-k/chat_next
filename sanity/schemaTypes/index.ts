51b1f93c81344b9aee568c3c7da0ff4729900c5d
Mikiale <mikialekesate@gmail.com>
2024-11-14 05:53:53 +0300

    adding again new sanity at night

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,10 +1,12 @@
 import { type SchemaTypeDefinition } from 'sanity'
 
-import {blockContentType} from './blockContentType'
-import {categoryType} from './categoryType'
-import {postType} from './postType'
-import {authorType} from './authorType'
+import { blockContentType } from './blockContentType'
+import { categoryType } from './categoryType'
+import { postType } from './postType'
+import { authorType } from './authorType'
+import { author } from '@/sanity/schemaTypes/author'
+import { startup } from './startup'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blockContentType, categoryType, postType, authorType],
+  types: [author, startup, blockContentType, categoryType, postType, authorType],
 }
