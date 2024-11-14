11955aff6d392a08f267827d379deb7da8a8d451
Mikiale <mikialekesate@gmail.com>
2024-11-14 13:42:30 +0300

    all changes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,5 @@
-import StartupCard from "@/components/StartupCard";
-import SearchForm from "../../components/SearchForm";
+import StartupCard ,{StartupCardType}from "@/components/StartupCard";
+import SearchForm  from "../../components/SearchForm";
 import { client } from "@/sanity/lib/client";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
@@ -31,7 +31,7 @@ export default async function Home({
         </p>
         <ul className="mt-7 card_grid">
           {posts?.length > 0 ? (
-            posts.map((post: StartupCardType, index: number) => (
+            posts.map((post: StartupCardType) => (
               <StartupCard key={post?._id} post={post} />
             ))
           ) : (
